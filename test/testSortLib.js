e0478b0ccb18004b52b1ffa8a23a69a3e4281206
Shankara <=>
2019-12-27 11:00:26 +0530

    modified test cases of sortLib

--- test/testSortLib.js
+++ test/testSortLib.js
@@ -27,17 +27,9 @@ describe("#performStreamSort", function() {
         done();
       }
     };
-    const ioUtils = {
-      fs: {
-        createReadStream: function() {
-          return new eventEmitter();
-        }
-      },
-      inputStream: new eventEmitter()
-    };
     const error = new Error("sort: No such file or directory");
     error.code = "ENOENT";
-    const inputStream = ioUtils.fs.createReadStream("badfile");
+    const inputStream = new eventEmitter();
     performStreamSort(inputStream, outputLoggers);
     inputStream.emit("error", error);
   });
@@ -50,67 +42,10 @@ describe("#performStreamSort", function() {
         done();
       }
     };
-    const ioUtils = {
-      fs: {
-        createReadStream: function() {
-          return new eventEmitter();
-        }
-      },
-      inputStream: new eventEmitter()
-    };
     const error = new Error("sort: unknown error");
     error.code = "UNKNOWN";
-    const inputStream = ioUtils.fs.createReadStream("file");
+    const inputStream = new eventEmitter();
     performStreamSort(inputStream, outputLoggers);
     inputStream.emit("error", error);
-  });
-});
-
-describe("#performSort", function() {
-  it("should perform sorting on given file contents", function() {
-    const userArgs = ["", "", "file"];
-    const outputLoggers = {
-      printSortedText: function(textLines) {
-        assert.strictEqual(textLines, "go\nhello");
-      },
-      printError: function(errorMsg) {}
-    };
-    const ioUtils = {
-      fs: {
-        createReadStream: function() {
-          return new eventEmitter();
-        }
-      }
-    };
-    const inputStream = ioUtils.fs.createReadStream();
-    performSort(userArgs, ioUtils, outputLoggers);
-    inputStream.emit("data", "b\n");
-    inputStream.emit("data", "c\n");
-    inputStream.emit("data", "a\n");
-    inputStream.emit("end");
-  });
-
-  it("should give stdin sorted if no file name is given", function() {
-    const userArgs = ["", ""];
-    const outputLoggers = {
-      printSortedText: function(text) {
-        assert.strictEqual(text, "a\nb\nc");
-      },
-      printError: function(errorMsg) {}
-    };
-    const ioUtils = {
-      fs: {
-        createReadStream: function() {
-          return new eventEmitter();
-        }
-      },
-      inputStream: new eventEmitter()
-    };
-    const inputStream = ioUtils.fs.createReadStream();
-    performSort(userArgs, ioUtils, outputLoggers);
-    inputStream.emit("data", "b\n");
-    inputStream.emit("data", "c\n");
-    inputStream.emit("data", "a\n");
-    inputStream.emit("end");
   });
 });
